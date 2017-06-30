a0d575a842714cf2f5b83e6df28679fcfa30b877
Gustavo Coelho <gutorc@hotmail.com>
2017-06-30 10:54:36 -0300

    Alteracoes
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- get_artigos.py
+++ get_artigos.py
@@ -4,27 +4,20 @@ from bs4.element import NavigableString
 import re
 from myPyArango import saveDocument, createRelacao
 
-fn = open('./DEL2848compilado.html', 'r')
-text = fn.read()
-soup = BeautifulSoup(text, 'html.parser')
-ps = soup.find_all('p')
-arg_pass = None
-for p in ps:
-    t = p.string
-    if type(t) is NavigableString:
-        t = str(t)
-        t = t.replace("\n", "")
-        #print("Linha: ", t)
-        m = re.search('(Art\. \d+)\s+-\s+(.*)', t) 
-        if(m is not None):
-            arg_pass = m.group(1)
-            #print(m.group(1))
-            #print(m.group(2))
-            #print("Achou",t)           
-            arg_pass = saveDocument(m.group(2), m.group(1))
-        m = re.search('(§ \d+)º\s+-\s+(.*)',t)
-        if(m is not None):
-            print("artigo", m.group(1))  
-            print("texto:",  m.group(2))  
-            doc = saveDocument(m.group(2), m.group(1))
-            createRelacao(arg_pass, doc, "pertence")
+def readText(filename):
+    f = open(filename, "r")
+    t = f.read()
+    return t
+
+def findArt(t):
+    m = re.search('(Art\. \d+)\s+-\s+(.*)', t) 
+    if(m is not None):
+        arg_pass = m.group(1)
+        print(m.group(1))
+        print(m.group(2))
+        #print("Achou",t)           
+    
+if __name__ == "__main__":
+    t = readText("resultado.txt")
+    print(t)
+    findArt(t)
